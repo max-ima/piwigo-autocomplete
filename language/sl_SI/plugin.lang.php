14169e17813b9e6776d31aae66f8134e2848b605
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-06 09:34:18 +0100

    New language SI

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+$lang['Configuration'] = 'Konfiguracija';
+$lang['Exclude'] = 'Izločiti';
+$lang['Custom'] = 'Po meri';
+$lang['Variants'] = 'Različice';
+
+//Configuration
+$lang['If you change album/tag names or permalink you might want to'] = 'Če spremenite imena albumov/oznak ali stalne povezave, boste morda želeli ';
+$lang['Invalidate auto completion index cache'] = 'Razveljavi predpomnilnik indeksa samodejnega dokončanja';
+$lang['(Auto completions are automatically invalidated on uploads, permission changes ...).'] = '<br> (Samodejni zaključki so samodejno razveljavljeni pri nalaganjih, spremembah dovoljenj ...).';
+
+//custom
+$lang['Query'] = 'Poizvedba';
+$lang['Use <b>\</b> to break search (e.g. Central Park\, NY will not match NY but it will display it)'] = 'Uporabite <b>\</b> za prekinitev iskanja (npr. Central Park\, NY se ne bo ujemal z NY, vendar ga bo prikazal)';
+$lang['Weight'] = 'Teža';
+$lang['Url'] = 'Url';
+$lang['Leave empty for automatic gallery search <br>OR enter an url ($r/ at the start of url means this gallery)<br>OR enter q=Term to search for a specific term'] = 'Pustite prazno za samodejno iskanje po galeriji <br>ALI vnesite url ($r/ na začetku url pomeni to galerijo)<br>ALI vnesite q=Izraz za iskanje določenega izraza';
+$lang['Create a new suggestion'] = 'Ustvarite nov predlog';
+$lang['In'] = 'Noter';//
+$lang['Type'] = 'Tip';//
+$lang['Out'] = 'Ven';//
+$lang[''] = '';//
+$lang[''] = '';//
+$lang[''] = '';//
+
+//variants
+$lang['Create new variants'] = 'Ustvarite nove različice';
+$lang['When any of the following words is entered'] = 'Ko je vnesena katera koli od naslednjih besed';
+$lang['Ignore it and'] = 'Ignorirajte in';
+$lang['Search it and'] = 'Išči in';
+$lang['Search also for any of the following'] = 'Poiščite tudi karkoli od naslednjega';
+$lang[''] = '';//
+$lang[''] = '';//
+$lang[''] = '';//
+$lang[''] = '';//
+$lang[''] = '';//
+$lang[''] = '';//
+?>
